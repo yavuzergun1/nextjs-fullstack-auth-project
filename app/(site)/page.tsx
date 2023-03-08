cbfcc2f73f4c0a2f6e38275860455649950cc2bf
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-08 14:54:04 +0300

    components added

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section>
+      <div className="container py-4">
+        <h1 className="text-2xl font-semibold tracking-tight">Home page</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
