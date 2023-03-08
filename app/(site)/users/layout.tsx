90823fd3d9470396a711fe23d6d103847f95c918
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-08 16:19:59 +0300

    users showed at the side bar

---
+++ app/(site)/users/layout.tsx
@@ -0,0 +1,33 @@
+
+import { getUsers } from "@/lib/prisma/users";
+import Users from "./users";
+
+// export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
+
+
+async function getData() {
+  const { users } = await getUsers();
+  if (!users) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return users;
+}
+
+const UsersLayout = async ({ children } : any) => {
+  const users = await getData();
+
+  return (
+    <section className="flex">
+          <aside className="w-1/3 lg:w-1/4">
+              {/* @ts-ignore */}
+        <Users users={users} />
+      </aside>
+      <main>{children}</main>
+    </section>
+  );
+};
+
+export default UsersLayout;
---
+++ app/(site)/users/users.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+// import { getUsers } from '@/lib/prisma/users'
+
+type Users = {
+  users: { id: string; name: string };
+};
+type User = {
+  user: { id: string; name: string };
+};
+const Users = async ({ users }: Users) => {
+  // const { users } = await getUsers()
+
+  return (
+    <section className="bg-rose-900">
+      <div className="container py-4">
+        <h2 className="mb-4 pb-3 text-xl border-b border-stone-500 font-medium text-white">
+          Users
+        </h2>
+        <ul className="flex flex-col text-sm text-white list-none gap-1 list-inside">
+          {/* @ts-ignore */}
+          {users?.map((user: any) => (
+            <li key={user.id} className="text-base">
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Users;
